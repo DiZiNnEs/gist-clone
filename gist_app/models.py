bb9c7a35c6d8cc76588605dd93fbf23ac0aaf6ef
dizinnes <dizinnes228@gmail.com>
2021-05-26 21:47:23 +0600

    feat(app/models): Create models for Gist

--- gist_app/models.py
+++ gist_app/models.py
@@ -1,2 +1,17 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
+
+class Gist(models.Model):
+    title = models.CharField(max_length=64, verbose_name='Gist name')
+    pub_date = models.DateTimeField(verbose_name='Publication date')
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
+
+    class Meta:
+        db_table = 'gist'
+        verbose_name = 'gist'
+        verbose_name_plural = 'gists'
+        ordering = ['title']
