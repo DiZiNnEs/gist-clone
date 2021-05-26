5337667ec1524f947ad3710b600a52caf33fe498
dizinnes <dizinnes228@gmail.com>
2021-05-26 21:48:09 +0600

    feat(app/admin): Register model in admin panel

--- gist_app/admin.py
+++ gist_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from gist_app.models import Gist
+
+
+@admin.register(Gist)
+class GistAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    list_filter = ('title', 'user', 'slug')
+    prepopulated_fields = {'slug': ('title',), }
