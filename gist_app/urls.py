0035123072bb514994693cb1cfcc9a148e554b6a
dizinnes <dizinnes228@gmail.com>
2021-05-28 21:34:31 +0600

    feat(app/views): Create detail views for gist

--- gist_app/urls.py
+++ gist_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from gist_app.views import IndexGistView
+from gist_app.views import IndexGistView, GistDetailView
 
 urlpatterns = [
     path('', IndexGistView.as_view(), name='index-gist'),
+    path('gist/<slug:slug>/', GistDetailView.as_view(), name='detail-gist'),
 ]
--- gist_app/views.py
+++ gist_app/views.py
@@ -6,3 +6,14 @@ from gist_app.models import Gist
 class IndexGistView(generic.ListView):
     queryset = Gist.objects.all()
     template_name = 'index.html'
+
+
+class GistDetailView(generic.DetailView):
+    queryset = Gist.objects.all()
+    template_name = 'detail-gist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = kwargs.get('object').slug
+        context['detail_object'] = self.queryset.filter(slug=slug)
+        return context
