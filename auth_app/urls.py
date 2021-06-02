90020b94ac9e7e86c00190df5a82b5a2c08a6ca5
dizinnes <dizinnes228@gmail.com>
2021-06-02 12:55:55 +0600

    feat(auth_app/views): Create `sign-in` form

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from auth_app.views import CreateUserView
+from auth_app.views import CreateUserView, LoginUserView
 
 urlpatterns = [
     path('sign-up/', CreateUserView.as_view(), name='sign-up'),
+    path('sign-in/', LoginUserView.as_view(), name='sign-in'),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from auth_app.forms import UserForm
@@ -9,4 +9,10 @@ class CreateUserView(generic.CreateView):
     model = CustomUser
     form_class = UserForm
     template_name = 'auth/sign-up.html'
-    success_url = '/'  # change to login
+    success_url = reverse_lazy('sign-in')
+
+
+class LoginUserView(generic.FormView):
+    form_class = UserForm
+    template_name = 'auth/sign-in.html'
+    success_url = '/'
