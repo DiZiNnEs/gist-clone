e21cd03a1a4ef6c3fd7b05417e13d5abedcc5f47
dizinnes <dizinnes228@gmail.com>
2021-06-04 19:20:51 +0600

    refactor(auth_app/views): Rewrite sign-in logic

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -16,3 +19,25 @@ class LoginUserView(generic.View):
     form_class = UserForm
     template_name = 'auth/sign-in.html'
     success_url = reverse_lazy('index-gist')
+
+    def get(self, request):
+        context = {
+            'form': self.form_class
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(self.success_url)
+        elif user is None:
+            messages.error(request, 'username or password not correct')
+            return redirect('sign-in')
+        else:
+            messages.error(request, 'some problems in server, sorry')
+            return redirect('sign-in')
+
